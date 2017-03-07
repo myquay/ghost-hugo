4deafec4fde7ba02bb2885673c1238cc743fad1b
Michael McKenna <myquay@gmail.com>
2017-03-08 12:50:42 +1300

    Encoding + schema update

--- ghost-hugo.py
+++ ghost-hugo.py
@@ -4,25 +4,29 @@ from datetime import datetime
 
 def main():
     # Read the Ghost export file
-    f = open('GhostData.json', 'r')
-    all_posts = json.load(f)['data']['posts']
+    f = open('GhostData.json', encoding="utf8")
+    all_posts = json.load(f)['db'][0]['data']['posts']
     f.close()
 
     for post in all_posts:
         # Create or use a folder for the date
-        created = datetime.strptime(post['created_at'], "%Y-%m-%dT%H:%M:%S.000Z")
+        try:
+            created = datetime.utcfromtimestamp(post['created_at']/1000) #, "%Y-%m-%dT%H:%M:%S.000Z")
+        except:
+            created = datetime.strptime(post['created_at'], "%Y-%m-%d %H:%M:%S")
+
         if not os.path.exists('./output/%d/%02d/%02d' % (created.year, created.month, created.day)):
             os.makedirs('./output/%d/%02d/%02d' % (created.year, created.month, created.day))
 
         # Create the Markdown file
-        pf = open('./output/%d/%02d/%02d/%s.md' % (created.year, created.month, created.day, post['slug']), 'w')
+        pf = open('./output/%d/%02d/%02d/%s.md' % (created.year, created.month, created.day, post['slug']), 'w', encoding="utf8")
 
         # Format the permalink
         post['url'] = '/%d/%02d/%02d/%s' % (created.year, created.month, created.day, post['slug'])
 
         # Front matter
         pf.write('+++\n')
-        pf.write('title = "%s"\n'.encode('utf8') % post['title'].replace('"', '\\"'))
+        pf.write('title = "%s"\n' % post['title'].replace('"', '\\"'))
         pf.write('slug = "%s"\n' % post['slug'])
         pf.write('date = "%s"\n' % post['created_at'])
         pf.write('url = "%s"\n' % post['url'])
@@ -31,7 +35,7 @@ def main():
         pf.write('+++\n\n')
 
         # Post body
-        pf.write(post['markdown'].encode('utf8'))
+        pf.write(post['markdown'])
 
         # Close the Markdown file
         pf.close()
